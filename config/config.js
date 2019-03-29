d1663507e351667b52f3892c415e213c277b9b9e
diegotony <tucotony1396@gmail.com>
2019-03-29 16:46:13 -0500

    bd

--- config/config.js
+++ config/config.js
@@ -13,12 +13,19 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 // BD
 //====================================================
 
-let urlDB = "mongodb://admin:admin123@ds031895.mlab.com:31895/sga";
-// if (process.env.NODE_ENV == 'dev') {
-//     urlDB = 'mongodb://localhost:27017/arquitectura';
-// } else {
-//     urlDB = process.env.MONGO_URI;
-// }
 
-urlDB = "mongodb://admin:admin123@ds031895.mlab.com:31895/sga"
+let urlDB;
+if (process.env.NODE_ENV == 'dev') {
+    urlDB = 'mongodb://localhost:27017/sga';
+
+    //mongodb://admin:admin123@ds031895.mlab.com:31895/sga
+} else {
+    urlDB = process.env.MONGO_URI;
+}
 process.env.URLDB = urlDB;
+
+
+// ====================================================
+// TOKEN
+// ================================= ===================
+process.env.CADUCIDAD_TOKEN = '144h';
